1741694c31375b0b621ae17dff8b113a86e040d8
joseVitor03 <jv681033@gmail.com>
2024-03-18 18:19:06 -0300

    remove pasta build

--- build/utils/brandCars.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const brandCars = ['VOLKSWAGEN', 'CHEVROLET', 'FIAT',
-    'FORD', 'TOYOTA', 'HYUNDAI', 'HONDA', 'RENAULT', 'NISSAN', 'JEEP'];
-exports.default = brandCars;
